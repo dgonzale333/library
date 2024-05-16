0c257cace1fd39f73971671c863310b576f3270c
unknown <SenaManana@ARMDFPCCIFSD050.FormacionSena.RED>
2024-05-16 09:00:59 -0500

    clase libro creada

---
+++ library.js
@@ -0,0 +1,24 @@
+class libro {
+    constructor(titulo, autor, genero, idioma, precio, formato, isbn, descripcion, estado, ubicacion, fecha_puclicacion,
+        editorial, paginas, dimensiones, peso) 
+    {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        this.idioma = idioma;
+        this.precio = precio;
+        this.formato = formato;
+        this.isbn = isbn;
+        this.descripcion = descripcion;
+        this.estado = estado;
+        this.ubicacion = ubicacion;
+        this.fecha_puclicacion = fecha_puclicacion;
+        this.editorial = editorial;
+        this.paginas = paginas;
+        this.dimensiones = dimensiones;
+        this.peso = peso;
+    }
+}
+
+
+
